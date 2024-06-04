5f512908c1726f7c3c91e6b995dc08a61040c3ff
MariniDwi <141748278+MariniDwi@users.noreply.github.com>
2024-06-04 20:28:48 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
-// function
+// function toggle menu
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+//function close menu
+document.addEventListener('click',
+    function(event) {
+        if (!menuToggle.contains(event.target) && !nav.contains(event.target)) {
+            nav.classList.remove('slide');
+        }
+    }
+);
+
+// function close toggle menu
+document.addEventListener('click', function(event) {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const checkbox = document.querySelector('.menu-toggle input');
+  
+    if (checkbox.checked && !menuToggle.contains(event.target)) {
+        checkbox.checked = false;
+      }
+  });
